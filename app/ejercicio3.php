406105c957ec1a1ba36f885a22e430bfa2e34086
cecilia <muracecilia@gmail.com>
2021-04-17 19:17:28 -0300

    Ejercicio3

---
+++ app/ejercicio3.php
@@ -0,0 +1,16 @@
+<?php   
+    
+	$mes=rand(1,12);
+	echo "El mes es: ".$mes "<br>";
+
+	if($mes>=4 && $mes<=6){
+		echo "Estamos en Otoño";
+	}elseif($mes>=7 && $mes<=9){
+		echo "Estamos en Invierno";
+	}elseif($mes>=10 && $mes<=12){
+		echo "Estamo en Primavera";
+	}else{
+		echo "Estamos en Verano";
+	}
+
+?>
